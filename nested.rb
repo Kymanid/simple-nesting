e5f99d7a1148068b5b04f1617abac04b79238813
Kymani Davis <Kymanidavis@gmail.com>
2020-11-22 18:56:40 -0500

    commit tree 0a55ab8a1f416b78d29be9591f2658a435347b8a

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+      return programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,7 +57,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     array = programmer_hash[:dennis_ritchie][:languages]
+     array.join
 end
+
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -79,8 +85,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+     programmer_hash[:yukihiro_matsumoto] = {:known_for =>  "Ruby", :languages  => ["LISP", "C"]}
+     programmer_hash
 end
 
 def changing_alan
@@ -102,6 +108,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash
 end
